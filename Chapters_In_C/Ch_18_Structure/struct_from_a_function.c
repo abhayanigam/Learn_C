0fcacb6358e10bf3b44064c6631f0765404c46d6
Abhaya Nigam <abhaya.nigam13@gmail.com>
2021-05-13 17:54:00 +0530

    Chapters in C

---
+++ Chapters_In_C/Ch_18_Structure/struct_from_a_function.c
@@ -0,0 +1,36 @@
+// Return struct from a function in C.
+
+#include <stdio.h>
+struct student
+{
+    char name[50];
+    int age;
+};
+
+// function prototype
+struct student getInformation();
+
+int main()
+{
+    struct student s;
+
+    s = getInformation();
+
+    printf("\nDisplaying information\n");
+    printf("Name: %s", s.name);
+    printf("\nRoll: %d", s.age);
+    
+    return 0;
+}
+struct student getInformation() 
+{
+  struct student s1;
+
+  printf("Enter name: ");
+  scanf ("%[^\n]%*c", s1.name);
+
+  printf("Enter age: ");
+  scanf("%d", &s1.age);
+  
+  return s1;
+}
